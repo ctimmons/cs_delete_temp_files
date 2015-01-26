4de9366dd6de851b64ae67295e2c3589e5b45ec4
ctimmons <chris@crtimmonsinc.com>
2015-01-25 18:43:36 -0600

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿/* This software is in the public domain.  See the LICENSE file for details. */
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Management;
+
+namespace DeleteTempFiles
+{
+  /* Conservatively delete all files and folders in the temp folder that were created before the last boot. */
+
+  public class Program
+  {
+    private static readonly DateTime _bootDateTime = GetBootDateTime();
+
+    public static void Main(String[] args)
+    {
+      RecursivelyDeleteTempFilesAndFolders(new DirectoryInfo(Path.GetTempPath()));
+    }
+
+    private static void RecursivelyDeleteTempFilesAndFolders(DirectoryInfo di)
+    {
+      foreach (var fsi in di.EnumerateFileSystemInfos())
+      {
+        if (fsi is DirectoryInfo)
+          RecursivelyDeleteTempFilesAndFolders(fsi as DirectoryInfo);
+        
+        if (CanDelete(fsi))
+        {
+          try
+          {
+            fsi.Delete();
+          }
+          catch
+          {
+            /* The file or folder can't be deleted.
+
+               Assume some other process is using this item.
+               Don't try to figure out why it can't be deleted or try to force
+               the deletion.  It's safest to just skip it. */
+          }
+        }
+      }
+    }
+
+    private static Boolean CanDelete(FileSystemInfo fsi)
+    {
+      return
+        (fsi.CreationTime   < _bootDateTime) &&
+        (fsi.LastAccessTime < _bootDateTime) &&
+        (fsi.LastWriteTime  < _bootDateTime) &&
+        ((fsi is DirectoryInfo) ? !(fsi as DirectoryInfo).EnumerateFileSystemInfos().Any() : true);
+    }
+
+    private static String GetWmiPropertyValueAsString(String queryString, String propertyName)
+    {
+      var query = new SelectQuery(queryString);
+      var searcher = new ManagementObjectSearcher(query);
+      foreach (ManagementObject mo in searcher.Get())
+        return mo.Properties[propertyName].Value.ToString();
+
+      return null;
+    }
+
+    private static DateTime GetWmiPropertyValueAsDateTime(String queryString, String propertyName)
+    {
+      var value = GetWmiPropertyValueAsString(queryString, propertyName);
+      return (value == null) ? DateTime.MinValue : ManagementDateTimeConverter.ToDateTime(value);
+    }
+
+    public static DateTime GetBootDateTime()
+    {
+      return GetWmiPropertyValueAsDateTime("SELECT * FROM Win32_OperatingSystem WHERE Primary='true'", "LastBootUpTime");
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # cs_delete_temp_files
-C# app to delete old files and folders from the current user's temp folder.
+
+#####C# app to delete old files and folders from the current user's temp folder.  
+
+>!!! CAUTION !!!
+
+>This app deletes files and folders.  If it deletes a file or folder that another process depends on, that process may crash or experience unexpected behavior.
+
+As a software developer, I tend to accumulate a lot of flotsam in my temp folder, usually from testing new software I've written.  Just prior to writing this app, my temp folder had thousands of files taking up 3 GB of drive space.
+
+The app assumes any file or folder that was created, accessed, or written to before the computer was booted up is an orphan, and can safely be deleted.  The app recursively walks the current user's temp folder depth first.  As the recursion unwinds, candidate files are deleted, as well as their parent folders (if the folder is empty).  If the file or folder can't be deleted, it is skipped.
+
+
+
